79f55d6ca2a6e965c627259d27e75d863b2af6ee
Miguel Arias <arias.miguelm@gmail.com>
2022-10-19 21:29:58 -0500

    create component FormFilter

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
+import Footer from '../Footer';
+import FormFilter from "../FormFilter";
+import Header from '../Header';
 import './App.css';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
-     <h1>hello</h1>
+     <Header/>
+     <FormFilter/>
+     <Footer/>
     </div>
+  </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/FormFilter/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function FormFilter(){
+ return(
+   <form>
+    <h2>
+        Busca ofertas en hoteles,
+        casas y mucho mas
+    </h2>
+    <input placeholder=''/>
+    <input placeholder=''/>
+    <button>Buscar</button>
+   </form>
+ );
+}
+export default FormFilter;
