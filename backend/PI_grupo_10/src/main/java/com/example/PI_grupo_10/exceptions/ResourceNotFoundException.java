798b25107cafa8f863347a29eee5bab2d680d83c
Hernan-Diaz <diazh@live.com.ar>
2022-10-20 22:56:26 -0300

    CRUD categorias - Crear API - Agregar endpoint de categorias

---
+++ backend/PI_grupo_10/src/main/java/com/example/PI_grupo_10/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.PI_grupo_10.exceptions;
+
+public class ResourceNotFoundException extends Exception{
+
+    public ResourceNotFoundException(String m) {
+        super(m);
+    }
+}
