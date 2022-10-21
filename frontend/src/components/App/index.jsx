88fb6324ef065f3ea88726ec7ac43c492ddb0e8c
Miguel Arias <arias.miguelm@gmail.com>
2022-10-21 13:55:46 -0500

    Added categories card, json to static information and search component

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -3,6 +3,10 @@ import Footer from '../Footer';
 import FormFilter from "../FormFilter";
 import Header from '../Header';
 import './App.css';
+import categoriData from "../../mock/categories.json"
+import logdgingData from "../../mock/lodging.json"
+import Categories from "../Categories";
+import Recomend from "../Recomend";
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
     <div className="App">
      <Header/>
      <FormFilter/>
+     <h2 className="title_categories">Buscar por tipo de alojamiento</h2>
+     <Categories data={categoriData} />
+     <h2 className="title_recomend">Recomendaciones</h2>
+     <Recomend dataLodging={logdgingData}/>
      <Footer/>
     </div>
   </BrowserRouter>
--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -4,22 +4,22 @@ import logo1N from "../../assets/images/logo1N.png";
 
 function Header() {
     return (
-      <header className="header">
+        <header className="header">
             <div className="container_logo">
-                <div to="/">
+                <div className="header_logo" to="/">
                     <img src={logo1N} alt="Logo" className="_logo" />
                 </div>
                 <div to="/">
-                    <p className="header_slogan">Sentite como en tu hogar</p>
+                    <i className="header_slogan">Sentite como en tu hogar</i>
                 </div>
             </div>
             <div className="header_buttons">
-                            <button className="btn_header">Crear Cuenta</button>
-                            <button className="btn_header">Inciar Sesion</button>
-                               
+                <button className="btn_header">Crear Cuenta</button>
+                <button className="btn_header">Inciar Sesion</button>
+
             </div>
-      </header>
+        </header>
     );
-  }
-  
-  export default Header;
+}
+
+export default Header;
