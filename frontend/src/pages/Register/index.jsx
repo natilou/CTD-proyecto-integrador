6e2e89e97084e1dc10d3976193641e3203acb725
natilou <30585029+natilou@users.noreply.github.com>
2022-10-24 23:53:25 -0300

    Template y eventos para login y registro

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -2,17 +2,125 @@ import React from "react";
 import "./Register.css"
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { validateEmail, validatePasswordLength, validateEmailRegistered } from '../LogIn/utils'
+import { usersRegistered } from "../LogIn/constants";
+import Swal from 'sweetalert2';
+
 
 
 function Register() {
-  const showlogin = true 
-    return (
-      <div>
-        <Header showLogin={showlogin}/>
-        <h1>Register</h1>
-       <Footer/>
-      </div>
-    );
+
+  const showlogin = true; 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("")
+  const [displayEmailError, setDisplayEmailError] = useState("none");
+  const [displayPasswordError, setDisplayPasswordError] = useState("none");
+  const [passwordConfirmedError, setPasswordConfirmedError] = useState("none");
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  let navigate = useNavigate();
+
+
+  function handleOnChangeNombre(e){
+    setNombre(e.target.value);
   }
-  
-  export default Register;
+
+  function handleOnChangeApellido(e){
+    setApellido(e.target.value);
+  }
+
+  function handleOnChangeEmail(e){
+    let email = e.target.value;
+    validateEmail(email) ? setEmail(email) : setDisplayEmailError("block");
+  }
+
+  function handleOnChangePassword(e){
+    let password = e.target.value;
+    if(validatePasswordLength(password)){
+      setPassword(password)
+    } else {
+      setDisplayPasswordError("block")
+    }
+  }
+
+  function handleOnChangePasswordConfirmed(e){
+    let passwordConfirmed = e.target.value;
+    password !== passwordConfirmed ? setPasswordConfirmedError("block") : setConfirmedPassword(password)
+  }
+
+  function handleSubmit(){
+    if(email === "" || nombre === "" || apellido === "" || confirmedPassword === ""){
+      Swal.fire({
+        icon: 'error',
+        text: 'Todos los campos son obligatorios',
+      })
+    }
+    else if(!validateEmailRegistered(email)){
+      usersRegistered.push(
+        {
+          email: email,
+          username: `${nombre} ${apellido}`,
+          password: confirmedPassword
+        }
+      )
+      return navigate("/")
+    } else {
+      Swal.fire({
+        icon: 'error',
+        text: 'El usuario ya se encuentra registrado',
+      })
+    }
+  }
+  return (
+    <div>
+      <Header showLogin={showlogin} />
+        <section className="container-register">
+            <h2 className="title-register">Crear una cuenta</h2>
+            <div className="row-register-with-col">
+                <div className="col-register">
+                  <div className="row-register-with-col">
+                    <label className="label-register">Nombre</label>
+                  </div>
+                  <div className="row-register-with-col">
+                    <input className="input-register" type="text" required onChange={handleOnChangeNombre}/>
+                  </div>
+                </div>
+                <div className="col-register">
+                  <div className="row-register-with-col">
+                    <label className="label-register">Apellido</label>
+                  </div>
+                  <div className="row-register-with-col">
+                  <input className="input-register" type="text" required onChange={handleOnChangeApellido}/>
+                  </div>
+                </div>
+            </div>
+            <div className="row-register">
+              <label className="label-register">Correo electrónico</label>
+              <input className="input-register" type="email" required onChange={handleOnChangeEmail}/>
+              <p style={{display:displayEmailError}}>Correo electrónico inválido</p>
+            </div>
+            <div className="row-register">
+              <label className="label-register">Contraseña</label>
+              <input className="input-register" type="password" required onChange={handleOnChangePassword}/>
+              <p style={{display:displayPasswordError}}>La contraseña debe tener más de 6 caracteres</p>
+            </div>
+            <div className="row-register">
+              <label className="label-register">Confirmar contraseña</label>
+              <input className="input-register" type="password" required onChange={handleOnChangePasswordConfirmed}/>
+              <p style={{display:passwordConfirmedError}}>Las contraseñas no coinciden</p>
+            </div>
+            <button className="btn-register" onClick={handleSubmit}>Crear una cuenta</button>
+            <div className="alternative-register">
+              <span className="span-register">¿Ya tienes cuenta?</span>
+              <Link to="/login">Iniciar Sesión</Link>
+            </div>
+        </section>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Register;
