1270945fdb9209a25af9a9f089881fce3437fc82
natilou <30585029+natilou@users.noreply.github.com>
2022-10-24 23:53:25 -0300

    template de login y regiter sin css

--- frontend/src/pages/LogIn/index.jsx
+++ frontend/src/pages/LogIn/index.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import "./LogIn.css"
+import { Link } from "react-router-dom";
 
 
 function LogIn() {
     return (
       <div>
         <Header/>
-        <h1>inicio de sesion</h1>
+          <section className="container_login">
+              <h2>Iniciar Sesión</h2>
+              <label>Correo electrónico</label>
+              <input type="email"/>
+              <label>Constraseña</label>
+              <input type="password"/>
+              <button>Ingresar</button>
+              <p>¿Aún no tienes cuenta?</p><Link to="/register">Crear Cuenta</Link>
+          </section>
         <Footer/>
       </div>
     );
--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import "./Register.css"
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import { Link } from "react-router-dom";
 
 
 function Register() {
     return (
       <div>
         <Header/>
-        <h1>Register</h1>
+          <section className="container_register">
+                <h2>Crear una cuenta</h2>
+                <label>Nombre</label>
+                <input type="text"/>
+                <label>Apellido</label>
+                <input type="text"/>
+                <label>Correo electrónico</label>
+                <input type="email"/>
+                <label>Contraseña</label>
+                <input type="password"/>
+                <label>Confirmar contraseña</label>
+                <input type="password"/>
+                <button>Crear una cuenta</button>
+                <p>¿Ya tienes cuenta?</p><Link to="/login">Iniciar Sesion</Link>
+          </section>
        <Footer/>
       </div>
     );
