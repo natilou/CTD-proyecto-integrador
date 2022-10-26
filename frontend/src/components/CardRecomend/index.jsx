fbd6d79ee55977796082d63aa70199ce8989c6a6
Miguel Arias <arias.miguelm@gmail.com>
2022-10-25 20:14:43 -0500

    update cards

--- frontend/src/components/CardRecomend/index.jsx
+++ frontend/src/components/CardRecomend/index.jsx
@@ -1,18 +1,53 @@
 import React from "react";
 import "./CardRecomend.css";
+import iconStart from "../../assets/images/icons/iconStar.png";
 
-function CardRecomend({dataLodging}) {
+function CardRecomend({ dataLodging }) {
     console.log(dataLodging)
-   const { name, category,url_image} = dataLodging
+    const { name, category, url_image } = dataLodging
     //, score, label_score,
-      //    description,city,distance
+    //    description,city,distance
     return (
-        <article className="card_recomend">
-            <figure className="container_image_recomend" >
-                <img className="image_recomend" src={url_image} alt={name} />
+        <article className="card_recommend">
+            <figure className="container_image_recommend" >
+                <img className="image_recommend" src={url_image} alt={name} />
             </figure>
-            <h3 >{name}</h3>
-            <p>{category} hoteles</p>
+            <div className="container_section_score">
+                <div>
+                    <section className="container_title_star">
+                        <p>{category}</p>
+                        <ul className="icons_list">
+                            <li>
+                                <img src="https://res.cloudinary.com/dbdrkxooj/image/upload/v1666741925/DH-PI/Star_3_rd9iyd.png" alt="star" className="icon_star" />
+                            </li>
+                            <li>
+                                <img src={iconStart} alt="star" className="icon_star" />
+                            </li>
+                            <li>
+                                <img src={iconStart} alt="star" className="icon_star" />
+                            </li>
+                            <li>
+                                <img src={iconStart} alt="star" className="icon_star" />
+                            </li>
+                            <li>
+                                <img src={iconStart} alt="start" className="icon_start" />
+                            </li>
+                        </ul>
+                    </section>
+                    <h3 >{name}</h3>
+                </div>
+                <div>
+                    <div><p>8</p></div>
+                    <p>muy bueno</p>
+                </div>
+            </div>
+            <section>
+                <img src={iconStart} alt="star" className="icon_star" />
+                <p>metros</p>
+                <p>monstrar en el mapa</p>
+            </section>
+            <p> </p>
+            <botton></botton>           
         </article>
     );
 }
