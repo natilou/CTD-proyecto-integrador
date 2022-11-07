c8d5b1b5525f39cfa062a6db4f6d652da1c0060e
Hernan-Diaz <diazh@live.com.ar>
2022-11-07 20:40:39 -0300

    Configuración de la conexión de BdD en la nube

--- backend/PI_grupo_10/src/main/java/com/example/PI_grupo_10/PiGrupo10Application.java
+++ backend/PI_grupo_10/src/main/java/com/example/PI_grupo_10/PiGrupo10Application.java
@@ -2,7 +2,11 @@ package com.example.PI_grupo_10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
 @SpringBootApplication
 public class PiGrupo10Application {
 
@@ -10,4 +14,22 @@ public class PiGrupo10Application {
 		SpringApplication.run(PiGrupo10Application.class, args);
 	}
 
+}
+*/
+@SpringBootApplication
+public class PiGrupo10Application {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiGrupo10Application.class, args);
+	}
 }
