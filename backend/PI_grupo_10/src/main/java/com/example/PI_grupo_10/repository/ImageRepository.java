6e761d226e4591aa7f5708b2f2bcc090c1023a57
Hernan-Diaz <diazh@live.com.ar>
2022-11-07 00:05:18 -0300

    Image service, controller - Endpoint obtener imagenes de un producto

--- backend/PI_grupo_10/src/main/java/com/example/PI_grupo_10/repository/ImageRepository.java
+++ backend/PI_grupo_10/src/main/java/com/example/PI_grupo_10/repository/ImageRepository.java
@@ -1,7 +1,11 @@
 package com.example.PI_grupo_10.repository;
 
 import com.example.PI_grupo_10.model.Image;
+import com.example.PI_grupo_10.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository <Image, Integer> {
+    List<Image> findByProductId (int productId);
 }
