00527ae68482cc7e9eaff2a36e286485e25bf4d6
Miguel Arias <arias.miguelm@gmail.com>
2022-11-09 12:47:41 -0500

    politics styles.

--- frontend/src/pages/Product/index.jsx
+++ frontend/src/pages/Product/index.jsx
@@ -7,40 +7,53 @@ import "./Product.css"
 import Gallery from "../../components/Gallery";
 import iconStar from "../../assets/images/icons/iconStar1.png";
 
+
 function Product() {
     const [productImages, setProductImages] = useState([]);
+    const [features, setfeatures] = useState([])
     const [isLoading, setIsLoading] = useState(true);
-  const {id}= useParams()
-  const showLogin = true ;
-  const showLogout = true;
-  const showLine = true;
+    const { id } = useParams()
+    const { category } = useParams()
+    const showLogin = true;
+    const showLogout = true;
+    const showLine = true;
+    const urlFeaturesID = `http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/products/${id}/features`;
 
-  
-  useEffect(() => {
-    getImages();
-  }, []);
+    useEffect(() => {
+        fetch(urlFeaturesID)
+        .then((response) => response.json())
+        .then((cities) => setfeatures(cities))
+        
+      },[]);
 
-  async function getImages() {
-    setIsLoading(true);
-    try {
-      setIsLoading(true);
-      await fetch('http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/products/1/images')
-      .then((response) => response.json())
-      .then((data) =>
-      setProductImages(data));
-      setIsLoading(false);
-    } catch (error) {
-      console.log({ error });
-      setIsLoading(false);
-    }
-  }
+
+   /*   useEffect(() => {
+        getImages();
+      }, []);
+    
+      async function getImages() {
+        setIsLoading(true);
+        try {
+          setIsLoading(true);
+          await fetch('http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/products/1/images')
+          .then((response) => response.json())
+          .then((data) =>
+          setProductImages(data));
+          setIsLoading(false);
+        } catch (error) {
+          console.log({ error });
+          setIsLoading(false);
+        }
+      }*/
+    
+    
     return (
         <div className="main_container_product" data-testid="product-container">
             <Header showLogin={showLogin} showLogout={showLogout} showLine={showLine} />
             <div className="block_header" data-testid="product-header">
-                <div className="block_header_titles"  data-testid="product-title-container">
-                    <h3 className="block_d"  data-testid="product-title">HOTEL</h3>
-                    <h2 className="block_name"  data-testid="product-id">{id}</h2>
+                <div className="block_header_titles" data-testid="product-title-container">
+                    <h3 className="block_d" data-testid="product-title">{category}</h3>
+                    <h2 className="block_name" data-testid="product-id">{id}</h2>
                 </div>
                 <div className="icon_back" data-testid="product-icon-back">
                     <Link to="/" className="back_image">
@@ -77,59 +90,71 @@ function Product() {
                             </li>
                         </ul>
                     </div>
+                    <div className="container_number_header">
+                        <p className="number_score">8</p>
+                    </div>
                 </div>
             </div>
-                <div className="container_gallery" data-testid="product-gallery">
-         {
-            !isLoading ? (
-              <Gallery images={productImages}/>
-            ) : (
-              <div style={{ width: '90%',  height: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <span className="loading-spa">Loading...</span>
-              </div>
-            )
-          }
-
-         </div>
+            <div className="container_gallery" data-testid="product-gallery">
+                {
+                    !isLoading ? (
+                        <Gallery images={productImages} />
+                    ) : (
+                        <div style={{ width: '90%', height: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            <span className="loading-spa">Loading...</span>
+                        </div>
+                    )
+                }
+            </div>
             <div className="description_product">
                 <h2 className="title_description_product">Alojate en el corazon de Buenos Aires</h2>
-                <p className="text_description" >Esta situado a solo unas calles de la avenida Alvear, 
+                <p className="text_description" >Esta situado a solo unas calles de la avenida Alvear,
                     de la avenida Quintana, del parque Martin y del distrito de
-                    Recoleta. En las inmediaciones tambien hay varios lugares de interes, 
-                    como la calle Florida, e centro comercial Galerias Pacifico, la zona del 
+                    Recoleta. En las inmediaciones tambien hay varios lugares de interes,
+                    como la calle Florida, e centro comercial Galerias Pacifico, la zona del
                     Puerto Madero, la plaza de Mayo y el palacio Municipal.
                     Nuestro clientes dicen que esta parte de Buenos Aires es su favorita, segun
                     los comentorios independiente.
-                    El Hotel sofisticado de 4 estrellas que goza de una ubicacion tranquila, o poca 
-                    distancia de prestigiosas de arte, teatros, museos y zonas comerciales. Ademas hay WIFI 
-                    gratuita. El estrablecimiento sirve un desayuno variado de 07:00 a 10:30.           
-                    </p>
+                    El Hotel sofisticado de 4 estrellas que goza de una ubicacion tranquila, o poca
+                    distancia de prestigiosas de arte, teatros, museos y zonas comerciales. Ademas hay WIFI
+                    gratuita. El estrablecimiento sirve un desayuno variado de 07:00 a 10:30.
+                </p>
             </div>
             <div className="container_features">
-                <h2>¿Que ofrece este lugar?</h2>
-                <hr className="hr"/>
-                <p></p>
+                <h2 className="title_features">¿Que ofrece este lugar?</h2>
+                <hr className="hr" />
+                <div className="container_icon_features">
+                    {
+                        features &&
+                        features.map((item) => (
+                            <div className="icons_features" key={`${id}${item.name}`}>
+                                <img className="img_features" src={`https://res.cloudinary.com/dbdrkxooj/image/upload/v1667983955/DH-PI/${item.name}.png`} alt={`icon ${item.name}`} />
+                                <p>{item.name}</p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
             <h2 className="title_politics">Que tenes que saber</h2>
-            <hr className="hr"/>
+            <hr className="hr" />
             <div className="container_politics">
                 <div className="type_politic">
                     <h3>Normas de la casa</h3>
-                    <p className="politic">Check-out: 10:00</p>  
-                    <p className="politic">No se permiten fiestas</p> 
-                    <p className="politic">No fumar</p>             
+                    <p className="politic">Check-out: 10:00</p>
+                    <p className="politic">No se permiten fiestas</p>
+                    <p className="politic">No fumar</p>
                 </div>
                 <div className="type_politic">
                     <h3>Salud y seguridad</h3>
                     <p className="politic">se aplican las pautas de distanciamiento social y otras
-                        normas relacionadas con el coranavirus</p>  
+                        normas relacionadas con el coranavirus</p>
                     <p className="politic">Detector de humo</p>
-                    <p className="politic">Deposito de seguridad</p>              
+                    <p className="politic">Deposito de seguridad</p>
                 </div>
                 <div className="type_politic">
                     <h3>Politica de cancelacion</h3>
                     <p className="politic"> Agrega las fechas de tu viaje para obtener los detalles
-                         de cancelacion de esta estadia.</p>  
+                        de cancelacion de esta estadia.</p>
                 </div>
             </div>
             <Footer />
