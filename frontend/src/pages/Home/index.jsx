2132756ba7425861d0f6babe05e7db67e8fb856b
Miguel Arias <arias.miguelm@gmail.com>
2022-11-09 22:43:57 -0500

    add useEffect for filter categories

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState }  from "react";
-import "./Home.css"
-//import categoryData from "../../mock/categories.json"
-//import lodgingData from "../../mock/lodging.json"
+import React, { useEffect, useState } from "react";
+import "./Home.css";
 import Header from "../../components/Header";
 import FormFilter from "../../components/FormFilter";
 import Categories from "../../components/Categories";
@@ -10,70 +8,56 @@ import Footer from "../../components/Footer";
 
 
 function Home() {
-  const [sectionCategory,setSectionCategory ] = useState([]);
+  const [sectionCategory, setSectionCategory] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingData, setIsLoadingData] = useState(true);
   const [products, setProducts] = useState([]);
   const [cities, setCities] = useState([]);
-  //const [selectCategory, setSelectCategory] = useState("");
   const [poble, setPoble] = useState([]);
-  const showLogin = true ;
+  const showLogin = true;
   const showLogout = true;
   const showLine = true;
-  const urlCategories ='http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/categories';
-  const urlProducts ='http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/products';
-  const urlCities ='http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/cities';
- // const urlCategoriesId=`http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/products/categories/${Number(selectCategory)}`;
- 
+  const urlCategories = 'http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/categories';
+  const urlProducts = 'http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/products';
+  const urlCities = 'http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/cities';
 
-    useEffect(() => {
-      fetch(urlCategories)
-      .then((response) => response.json())
-      .then((categories) => setSectionCategory(categories))
-    },[]);
+  useEffect(() => {
+    getdata()
+  }, [])
 
-    useEffect(() => {
-      fetch(urlProducts)
-      .then((response) => response.json())
-      .then((products) => setProducts(products))
-      
-    },[]);
+  async function getdata() {
+    setIsLoadingData(true);
+    try {
+      setIsLoadingData(true);
 
-    useEffect(() => {
-      fetch(urlCities)
-      .then((response) => response.json())
-      .then((cities) => setCities(cities))
-      
-    },[]);
-      //console.log(products)
-    // console.log(sectionCategory)
-     //console.log(cities);
-  
-     /*const handleNameChange =(id)=>{
-      setSelectCategory(id); 
-      console.log(selectCategory)
-        };
-    */
-    /*useEffect(() => {
-        fetch(urlCategoriesId)
+      await fetch(urlCategories)
         .then((response) => response.json())
-        .then((cities) => setPoble(cities))
+        .then((categories) => setSectionCategory(categories));
 
-      },[])
-      console.log(poble)
-   console.log(Number(selectCategory))*/
+      await fetch(urlProducts)
+        .then((response) => response.json())
+        .then((products) => setProducts(products));
 
-   /*useEffect(() => {
-    getImages();
-  }, []);
-*/
+      await fetch(urlCities)
+        .then((response) => response.json())
+        .then((cities) => setCities(cities))
+
+      setIsLoadingData(false);
+    } catch (error) {
+      console.log({ error });
+      setIsLoadingData(false);
+    }
+  }
+
+
   async function getCategory(id) {
     setIsLoading(true);
     try {
       setIsLoading(true);
       await fetch(`http://ec2-3-21-197-14.us-east-2.compute.amazonaws.com:8080/products/categories/${Number(id)}`)
-      .then((response) => response.json())
-      .then((data) =>
-      setPoble(data));
+        .then((response) => response.json())
+        .then((data) => setPoble(data));
+
       setIsLoading(false);
     } catch (error) {
       console.log({ error });
@@ -81,27 +65,39 @@ function Home() {
     }
   }
   console.log(poble)
-    return (
-      <div data-testid="home-container">
-        <Header showLogin={showLogin} showLogout={showLogout} showLine={showLine}/>
-        <FormFilter cities={cities} />
-        <h2  className="title_categories" data-testid="home-title">Buscar por tipo de alojamiento</h2>
+  return (
+    <>
+      {
+        isLoadingData
+          ? (
+            <div style={{ width: '90%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', alignContent: 'center' }} >
+              <span className="loading-spa">Cargando...</span>
+            </div >)
+          : (
+            <div data-testid="home-container">
+              <Header showLogin={showLogin} showLogout={showLogout} showLine={showLine} />
+              <FormFilter cities={cities} />
+              <h2 className="title_categories" data-testid="home-title">Buscar por tipo de alojamiento</h2>
 
-        <Categories  data={sectionCategory} onclick={getCategory} />
+              <Categories data={sectionCategory} onclick={getCategory} />
 
-        {
-          !isLoading &&
-          <div>
-          <h2 className="main_title_recommedation" data-testid="home-title-2">hotels</h2>
-            <Recommendation dataLodging={poble}/>
-          </div>
-        }
-        <h2 className="main_title_recommedation" data-testid="home-title-2">Recomendaciones</h2>
-        <Recommendation dataLodging={products}/>
-        <Footer/>
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+              {
+                !isLoading &&(
+                    <div>
+                      <h2 className="main_title_recommedation" data-testid="home-title-2">{poble[0].category.title}</h2>
+                      <Recommendation dataLodging={poble} />
+                    </div>)
+              }
+              <h2 className="main_title_recommedation" data-testid="home-title-2">Recomendaciones</h2>
+              <Recommendation dataLodging={products} />
+              <Footer />
+            </div>
+          )
+
+      }
+    </>
+  );
+
+}
+
+export default Home;
