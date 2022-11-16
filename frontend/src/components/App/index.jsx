c7239dbb4f5f84226ae38dd2756f9abc053984b9
natilou <30585029+natilou@users.noreply.github.com>
2022-11-15 23:01:46 -0300

    Implementar el template de reserva exitosa, ya es funcional y solo falta el icono de check

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -4,6 +4,7 @@ import Home from "../../pages/Home";
 import Register from "../../pages/Register";
 import LogIn from "../../pages/LogIn";
 import Product from "../../pages/Product";
+import SuccessfulReservation from "../../pages/SuccessfulReservation";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route path='/register' element={<Register/>} />
             <Route path='/login' element={<LogIn/>} />
             <Route path="/product/:category/:id" element={<Product/>}/>
+            <Route path="/successful-reservation" element={<SuccessfulReservation/>}/>
         </Routes>
   </BrowserRouter>
   );
