6a21fbd9a34cd3c876996329221e14588ac77a91
natilou <30585029+natilou@users.noreply.github.com>
2022-11-16 22:25:09 -0300

    cambiando nombre a la ruta y al componente por SuccessfulBooking y modificando estilos responsive agregando test con jest para SuccessfulBooking

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -4,7 +4,7 @@ import Home from "../../pages/Home";
 import Register from "../../pages/Register";
 import LogIn from "../../pages/LogIn";
 import Product from "../../pages/Product";
-import SuccessfulReservation from "../../pages/SuccessfulReservation";
+import SuccessfulBooking from "../../pages/SuccessfulBooking";
 import Booking from "../../pages/Booking";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
             <Route path='/register' element={<Register/>} />
             <Route path='/login' element={<LogIn/>} />
             <Route path="/product/:category/:id" element={<Product/>}/>
-            <Route path="/successful-reservation" element={<SuccessfulReservation/>}/>
+            <Route path="/successful-booking" element={<SuccessfulBooking/>}/>
             <Route path="/reserva/:id" element={<Booking/>}/>
         </Routes>
   </BrowserRouter>
