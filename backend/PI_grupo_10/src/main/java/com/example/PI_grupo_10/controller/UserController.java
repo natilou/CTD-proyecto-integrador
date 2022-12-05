a6115e466cfbc174af1ce7ee38afaa1c5c2e0a6c
anibelisario <aniuska.alvarado@gmail.com>
2022-12-05 20:33:37 -0300

    crud registro usuario

---
+++ backend/PI_grupo_10/src/main/java/com/example/PI_grupo_10/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.PI_grupo_10.controller;
+
+import com.example.PI_grupo_10.model.User;
+import com.example.PI_grupo_10.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> registrarUsuario(@RequestBody User user){
+        return new ResponseEntity<>(userService.agregarUsuario(user), HttpStatus.CREATED);
+    }
+
+
+}
--- backend/PI_grupo_10/src/main/java/com/example/PI_grupo_10/service/UserService.java
+++ backend/PI_grupo_10/src/main/java/com/example/PI_grupo_10/service/UserService.java
@@ -3,18 +3,25 @@ package com.example.PI_grupo_10.service;
 import com.example.PI_grupo_10.model.User;
 import com.example.PI_grupo_10.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    private static final Logger logger = Logger.getLogger(UserService.class);
 
     public User getUserByNameAndPassword(String email, String password) {
         User user = this.userRepository.findByEmailAndPassword(email, password);
-        if(user == null){
+        if (user == null) {
             return null;
         }
         return user;
+    }
+
+    public User agregarUsuario(User user) {
+        logger.info("Se agrego el usuario: " + user);
+        return userRepository.save(user);
     }
 }
