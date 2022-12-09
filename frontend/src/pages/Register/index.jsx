04ac15069e387e1fb878bc7d8c8b470efd4010ac
natilou <30585029+natilou@users.noreply.github.com>
2022-12-09 13:38:29 -0300

    cambiando validaciones de registro

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -4,8 +4,8 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Formik, Field } from 'formik';
 import { validateEmailRegex, validatePasswordLength, validateEmailAndPassword, validatePasswordConfirmation } from '../LogIn/utils'
-import { usersRegistered } from "../LogIn/constants";
 import Swal from 'sweetalert2';
 
 
@@ -29,29 +29,58 @@ function Register() {
     </label>
   );
 
-
-  function handleOnChangeNombre(e){
-    setName(e.target.value)
+  const validateName = (value) => {
+    let error;
+    if (!value) {
+      error = 'Debes completar el campo del nombre';
+    } else {
+      setName(value)
+    }
+    return error;
   }
-
-  function handleOnChangeApellido(e){
-    setLastName(e.target.value);
+  const validateLastName = (value) => {
+    let error;
+    if (!value) {
+      error = 'Debes completar el campo del apellido';
+    } else {
+      setLastName(value)
+    }
+    return error;
   }
-
-  function handleOnChangeEmail(e){
-    let email = e.target.value;
-    setEmail(email)
+  const validateEmail = (value) => {
+    let error;
+    if (!value) {
+      error = 'Debes completar el campo del correo electronico';
+    } else if(!validateEmailRegex(value)){
+      error = 'El correo electrónico ingresado es inválido'
+    } else if (value && validateEmailRegex(value)){
+      setEmail(value);
+    }
+    return error;
+  };
+  
+  const validatePassword = (value) => {
+    let error;
+    if (!value) {
+      error = 'Debes completar el campo de la contraseña';
+    } else if (value.length < 6) {
+      error = 'La contraseña debe tener más de 6 caracteres';
+    } else if(value && value.length > 6){
+      setPassword(value);
+    }
+    return error;
   }
-
-  function handleOnChangePassword(e){
-    let password = e.target.value;
-    setPassword(password)
-  }
-
-  function handleOnChangePasswordConfirmed(e){
-    let passwordConfirmed = e.target.value;
-    setConfirmedPassword(passwordConfirmed)
-    
+  const validateConfirmedPassword = (value) => {
+    let error;
+    if (!value) {
+      error = 'Debes completar el campo de la contraseña';
+    } else if( !validatePasswordConfirmation(password, value)){
+      error = 'Las contraseñas no coinciden';
+    } else if (value && validatePasswordConfirmation(password, value)){
+      setConfirmedPassword(value);
+    }
+    return error;
+   
   }
 
   function handleSubmit(){
@@ -60,32 +89,7 @@ function Register() {
         icon: 'error',
         text: 'Todos los campos son obligatorios',
       })
-    }
-    else if(validateEmailAndPassword(email)){
-      Swal.fire({
-        icon: 'error',
-        text: 'El usuario ya se encuentra registrado',
-      })
-    }
-    else if(!validatePasswordConfirmation(password, confirmedPassword)){
-      Swal.fire({
-        icon: 'error',
-        text: 'Las contraseñas no coinciden',
-      })
-    }
-    else if(!validateEmailRegex(email)){
-      Swal.fire({
-        icon: 'error',
-        text: 'Correo electrónico inválido',
-      })
-    }
-    else if(!validatePasswordLength(password)){
-      Swal.fire({
-        icon: 'error',
-        text: 'La contraseña debe tener más de 6 caracteres',
-      })
-    }
-    else {
+    } else {
       let payload = {
         name: name,
         lastName: lastName,
@@ -122,56 +126,87 @@ function Register() {
   }
 
   return (
-    <div>
-      <Header showLogin={showlogin} />
-        <section className="container-register" data-testid="register-container">
+      <div>
+        <Header showLogout={showlogin} />
+          <section className="container-register" data-testid="register-container">
             <h2 className="title-register" data-testid="register-title">Crear una cuenta</h2>
-            <div className="row-register-with-col" data-testid="register-row-container-1">
-                <div className="col-register" data-testid="register-col-container-1">
-                  <div className="row-register-with-col" data-testid="register-row-container-2">
-                    <label className="label-register" data-testid="register-name-label">Nombre</label>
+              <Formik
+              initialValues={{
+                name: '',
+                lastName: '',
+                email: '',
+                password: '',
+                confirmedPassword: '',
+              }}
+              >
+              {({ errors, touched }) => (
+                <form className="form-register">
+                  <div className="row-register-with-col" data-testid="register-row-container-1">
+                    <div className="col-register" data-testid="register-col-container-1">
+                      <div className="row-register-with-col" data-testid="register-row-container-2">
+                        <label className="label-register" data-testid="register-name-label">Nombre</label>
+                      </div>
+                        <div className="row-register-with-col" data-testid="register-col-container-2">
+                          <Field name="name" type="text" validate={validateName} className="input-register" />
+                        </div>
+                    </div>
+                    <div className="col-register" data-testid="register-col-1">
+                      <div className="row-register-with-col" data-testid="register-row-container-3">
+                        <label className="label-register" data-testid="register-name-label">Apellido</label>
+                      </div>
+                      <div className="row-register-with-col" data-testid="register-row-container-4">
+                        <Field name="lastName" type="text" validate={validateLastName} className="input-register"/>
+                      </div>
+                    </div>
                   </div>
                   <div className="row-register-with-col" data-testid="register-col-container-2">
-                    <input className="input-register" type="text" required onChange={handleOnChangeNombre} data-testid="register-name-input"/>
+                      {errors.name && touched.name ? (
+                        <div className="error-input-register">{errors.name}</div>
+                      ) :  <div className="error-input-register"></div>}
+                      {errors.lastName && touched.lastName ? (
+                        <div className="error-input-register">{errors.lastName}</div>
+                      ) :  <div className="error-input-register"></div>}
                   </div>
-                </div>
-                <div className="col-register" data-testid="register-col-1">
-                  <div className="row-register-with-col" data-testid="register-row-container-3">
-                    <label className="label-register" data-testid="register-lastname-label">Apellido</label>
+                  <div className="row-register" data-testid="register-row-1">
+                    <label className="label-register" data-testid="login-password-label">Correo electrónico</label>
+                    <Field name="email" type="email" validate={validateEmail} className="input-register"/>
+                    {errors.email && touched.email ? (
+                      <div className="error-input-register">{errors.email}</div>
+                    ) :  <div className="error-input-register"></div>}
                   </div>
-                  <div className="row-register-with-col" data-testid="register-row-container-4">
-                  <input className="input-register" type="text" required onChange={handleOnChangeApellido} data-testid="register-lastname-input"/>
+                  <div className="row-register" data-testid="register-row-2"> 
+                    <label className="label-register" data-testid="login-password-label">Contraseña</label>
+                    <Field name="password" type="password" validate={validatePassword} className="input-register"/>
+                    {errors.password && touched.password ? (
+                      <div className="error-input-register">{errors.password}</div>
+                    ) :  <div className="error-input-register"></div>}
                   </div>
-                </div>
-            </div>
-            <div className="row-register" data-testid="register-row-1">
-              <label className="label-register" data-testid="register-email-label">Correo electrónico</label>
-              <input className="input-register" type="email" required onChange={handleOnChangeEmail} data-testid="register-email-input"/>
-            </div>
-            <div className="row-register" data-testid="register-row-2">
-              <label className="label-register" data-testid="register-password-label">Contraseña</label>
-              <input className="input-register" type="password" required onChange={handleOnChangePassword} data-testid="register-password-input"/>
-            </div>
-            <div className="row-register" data-testid="register-row-3">
-              <label className="label-register" data-testid="register-passconfirmation-label">Confirmar contraseña</label>
-              <input className="input-register" type="password" required onChange={handleOnChangePasswordConfirmed} data-testid="register-passconfirmation-input"/>
-            </div>
+                  <div className="row-register" data-testid="register-row-3">
+                    <label className="label-register" data-testid="login-password-label">Confirmar contraseña</label>
+                    <Field name="confirmedPassword" type="password" validate={validateConfirmedPassword} className="input-register"/>
+                    {errors.confirmedPassword && touched.confirmedPassword ? (
+                      <div className="error-input-register">{errors.confirmedPassword}</div>
+                    ) :  <div className="error-input-register"></div>}
+                  </div>
+                </form>
+              )}
+            </Formik>
             <div className="checkbox">
-            <Checkbox
-              checked={isAdmin}
-              onChange={() => setIsAdmin((state) => !state)}
-            >
-              Quiero registrarme para publicar productos
-            </Checkbox>
+              <Checkbox
+                checked={isAdmin}
+                onChange={() => setIsAdmin((state) => !state)}
+              >
+                Quiero registrarme para publicar alojamientos
+              </Checkbox>
             </div>
             <button className="btn-register" onClick={handleSubmit} data-testid="register-btn">Crear una cuenta</button>
             <div className="alternative-register" data-testid="register-alternative">
               <span className="span-register" data-testid="alternative-span">¿Ya tienes cuenta?</span>
               <Link to="/login" data-testid="login-link">Iniciar Sesión</Link>
             </div>
-        </section>
+          </section>
       <Footer/>
-    </div>
+     </div>
   );
 }
 
