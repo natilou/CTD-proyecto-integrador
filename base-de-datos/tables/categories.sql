fadd4caca0cd81b02af9b8baaa9aeb8b29ac37b9
natilou <30585029+natilou@users.noreply.github.com>
2022-12-14 21:57:13 -0300

    modificando typos

--- base-de-datos/tables/categories.sql
+++ base-de-datos/tables/categories.sql
@@ -8,4 +8,4 @@ CREATE TABLE
     UNIQUE KEY `id_UNIQUE` (`id`)
   ) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
 
-  ALTER TABLE  `categories` ADD COLUMN `product_amount` bigint NOT NULL;
+  ALTER TABLE  `categories` ADD COLUMN `product_amount` bigint NOT NULL
