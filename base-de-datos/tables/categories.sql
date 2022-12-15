593eda738a284119c3741a48de7c8744fd617981
natilou <30585029+natilou@users.noreply.github.com>
2022-12-14 21:55:21 -0300

    agregando scripts de modificaciones de tablas

--- base-de-datos/tables/categories.sql
+++ base-de-datos/tables/categories.sql
@@ -7,3 +7,5 @@ CREATE TABLE
     PRIMARY KEY (`id`),
     UNIQUE KEY `id_UNIQUE` (`id`)
   ) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+
+  ALTER TABLE  `categories` ADD COLUMN `product_amount` bigint NOT NULL;
